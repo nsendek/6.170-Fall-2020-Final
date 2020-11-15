55e28da3c492e6bab79b01c3017c89855f61cb21
Howard DaCosta <hdacosta@MacBook-Pro.fios-router.home>
2020-11-15 15:41:23 -0500

    vue skeleton for Zelp

---
+++ src/views/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class='entire-page'>
+    <div class='full-container'>
+        <p style="text-align: center;">Zelp Main Page</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "main",
+};
+</script>
