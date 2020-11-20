97289601c47cbfb0024952ac6310df6e4c0dee30
vjusiega <vjusiega@mit.edu>
2020-11-20 11:20:00 -0500

    map styling

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -8,11 +8,10 @@
         <BusinessFeed/>
       </div>
       <div class="map-pane">
-      <GmapMap
+      <GmapMap class="g-map"
         :center="{lat:42.3601, lng:-71.0942}"
         :zoom="15"
         map-type-id="roadmap"
-        style="width: 50vw; height: 100vh"
       >
       </GmapMap>
       </div>
