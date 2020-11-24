0ffe030d585df92326c4d3d706ccfecdb34fda87
nsendek <nsendek@dhcp-10-29-207-130.dyn.MIT.EDU>
2020-11-24 02:05:00 -0500

    cleaning

--- models/Businesses.js
+++ models/Businesses.js
@@ -37,11 +37,19 @@ class Businesses {
 
   /**
    * Confirm that business exists in the database.
-   * @param {number} id 
+   * @param {string | number} identifier - accountName or ID
    * @returns {boolean} whether the business exists or not
    */
-  static async exists(id) {
-    return Boolean(await Businesses.get(id));
+  static async exists(identifier) {
+    if (isNaN(identifier)) { // using accountName instead of ID
+      let db = await SQL.getDB();
+      let out = await db.get(`SELECT * FROM businesses WHERE accountName = $1`,[identifier]);
+      db.close();
+      return Boolean(out);
+    }
+
+    // identifer is ID
+    return Boolean(await Businesses.get(identifier));
   }
 
   /**
@@ -85,41 +93,8 @@ class Businesses {
 
     db.close();
 
-    console.log(res);
-
     if (res.error) return undefined;
     else return Businesses.get(res.lastID)
-  }
-
-  static async addAddress(address, businessId) {
-    let db = await SQL.getDB();
-    let out = {address, lat : 0, lng : 0}
-
-    // do geocoding of address to lat long
-    let data = await axios.get("https://maps.googleapis.com/maps/api/geocode/json", { 
-      params : { 
-          address : `${address}, Cambridge, MA, USA`,
-          key : "AIzaSyDKOlyw5FKzfofKtyQ5jfKFuleqelf1nhQ"
-        }
-    })
-      .then(data => data.data)
-      .catch(err => err.response ? err.response : err);
-
-    if (!data.results[0]) return undefined;
-    
-    out.address = data.results[0].formatted_address;
-    out.lat = data.results[0].geometry.location.lat;
-    out.lng = data.results[0].geometry.location.lng;
-
-    let res = await db.run(`UPDATE businesses 
-      SET address = $1, lat = $2, lng = $3 
-      WHERE id = $4`, [out.address, out.lat, out.lng, businessId])
-      .catch(SQL.parseError);
-
-    db.close();
-
-    if (res.error) return undefined;
-    else return await Businesses.get(res.lastID);
   }
 
   /**
@@ -143,7 +118,7 @@ class Businesses {
    * @param {string} newName - new name
    * @return {User | null} the altered User (minus password)
    */
-  static async changeName(id, newName) {
+  static async updateName(id, newName) {
     let out = null;
     let db = await SQL.getDB();
     let business = await Businesses.get(id);
@@ -163,7 +138,7 @@ class Businesses {
    * @param {string} newPassword - new password
    * @return {Business | null} the altered User (minus password)
    */
-  static async changePassword(id, newPassword) {
+  static async updatePassword(id, newPassword) {
     let out = null;
     let db = await SQL.getDB();
     let business = await Businesses.get(id);
@@ -173,6 +148,42 @@ class Businesses {
     }
     db.close();
     return out;
+  }
+
+  /**
+   * change/add address
+   * @param {string} id - id of business
+   * @param {string} address 
+   */
+  static async updateAddress(id, address) {
+    let db = await SQL.getDB();
+    let out = {address, lat : 0, lng : 0}
+
+    // do geocoding of address to lat long
+    let data = await axios.get("https://maps.googleapis.com/maps/api/geocode/json", { 
+      params : { 
+          address : address,
+          key : "AIzaSyDKOlyw5FKzfofKtyQ5jfKFuleqelf1nhQ"
+        }
+    })
+      .then(data => data.data)
+      .catch(err => err.response ? err.response : err);
+
+    if (!data.results || !data.results[0]) return undefined;
+    
+    out.address = data.results[0].formatted_address;
+    out.lat = data.results[0].geometry.location.lat;
+    out.lng = data.results[0].geometry.location.lng;
+
+    let res = await db.run(`UPDATE businesses 
+      SET address = $1, lat = $2, lng = $3 
+      WHERE id = $4`, [out.address, out.lat, out.lng, id])
+      .catch(SQL.parseError);
+
+    db.close();
+
+    if (res.error) return undefined;
+    else return Businesses.get(id);
   }
 }
 
