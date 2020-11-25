35060c55d6936599718532e537ab28cc7f242727
nsendek <nsendek@dhcp-10-29-207-130.dyn.MIT.EDU>
2020-11-24 20:46:51 -0500

    cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
     <EventHandler /> 
     <router-view/>
   </div>
