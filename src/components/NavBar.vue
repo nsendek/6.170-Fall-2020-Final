e828449769e8cc5d22704db10d036530a8fb78b0
nsendek <nsendek@dhcp-10-29-207-130.dyn.mit.edu>
2020-11-26 19:09:45 -0500

    moved navbar outside of <router-view>

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="navbar">
-    <div><router-link to="/"> ZELP </router-link></div> 
-    <div v-if="this.$state.username == ''"> 
+  <v-app-bar app flat height="65px">
+
+    <router-link to="/"> ZELP </router-link>
+    
+    <v-spacer></v-spacer>
+
+    <div v-if="!this.$state.username"> 
       <router-link to="/login"> LOG IN </router-link>  
     </div> 
-    <div v-else-if="this.$state.username != '' && !(this.$state.isBusiness)"> 
+    <div v-else-if="this.$state.username && !(this.$state.isBusiness)"> 
       <div> <router-link to="/user-profile"> {{this.$state.username}} </router-link></div>
     </div> 
-    <div v-else-if="this.$state.username != '' && (this.$state.isBusiness)">
+    <div v-else-if="this.$state.username && (this.$state.isBusiness)">
       <div> <router-link to="/business-profile"> {{this.$state.username}} </router-link></div>
     </div>
-  </div>
-</template>
 
+  </v-app-bar>
+</template>
 <script>
 
 export default {
   name : "NavBar", 
-
-  props : {
-    showUserInfo : {default : true}
-  }, 
 }
 </script>
--- src/views/BusinessProfilePage.vue
+++ src/views/BusinessProfilePage.vue
@@ -1,20 +1,15 @@
 <template>
-  <div>
-    <nav-bar />
-    <business-profile/>
-  </div>
+    <BusinessProfile/>
 </template>
 
 <script>
 import BusinessProfile from '../components/BusinessProfile.vue'
-import NavBar from "../components/NavBar.vue"
 
 export default {
   name : "UserProfileVue",
 
   components : {
     BusinessProfile, 
-    NavBar
   }
 }
 </script>
