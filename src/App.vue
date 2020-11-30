61e373e8f59a4a360578ab38f0b48be0dc67c722
nsendek <nsendek@dhcp-10-29-207-130.dyn.mit.edu>
2020-11-30 12:36:36 -0500

    added draggable panels

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,16 @@
     <EventHandler /> 
     <router-view name="overlay" />
     <v-main>
-      <v-row >
-        <v-col  class ="left-col">
-          <router-view class = "scroll-box"/>       
-        </v-col>
-        <v-col class ="right-col" >
+
+      <splitpanes class = "default-theme">
+        <pane min-size="30"> 
+          <router-view class = "scroll-box"/>  
+        </pane>
+        <pane min-size="30"> 
           <Map />
-        </v-col>
-      </v-row>
+        </pane>
+      </splitpanes>
+      
     </v-main>
   </v-app>
 </template>
@@ -20,11 +22,16 @@ import EventHandler from "./components/EventHandler.vue";
 import NavBar from "./components/NavBar";
 import Map from "./components/Map";
 
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
+
 export default {
   components : {
     EventHandler,
     NavBar,
-    Map
+    Map,
+    Splitpanes,
+    Pane
   }
 }
 </script>
@@ -39,25 +46,38 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background-color: lavender;
-  /* z-index: 0;  */
+  overflow: hidden !important; /* DON'T delete, prevents bug in safari */
+  z-index: 0; 
 }
 
-.left-col {
+body::-webkit-scrollbar {
+  display: none; /* bug where empty scrollbar is always visible */
+}
+
+.scroll-box {
+  height : calc(100vh - var(--navbar-height));
+  overflow-y: scroll; 
+}
+
+.splitpanes--vertical > .splitpanes__splitter {
+  min-width: 10px;
+}
+
+/* DON'T DELETE BELOW (in case splitpanes becomes a problem*/
+/* .left-col {
   padding-top: 0px ; 
   padding-right:0px ;
   padding-bottom:0px ;
 }
-
 .right-col {
   padding-top: 0px ;  
   padding-left:0px ; 
   padding-bottom:0px ;
-}
-.scroll-box {
-  /* DON'T delete below */
-  height : calc(100vh - var(--navbar-height));
-  overflow-y: scroll; 
-}
+} 
+ .split-pane {
+  height : calc(100vh - var(--navbar-height)); 
+  height : 100%;
+} */
 </style>
 
 
--- src/main.js
+++ src/main.js
@@ -5,10 +5,32 @@ import Vuetify from 'vuetify/lib';
 import App from './App.vue'
 import router from './router'
 import * as VueGoogleMaps from 'vue2-google-maps'
+
 import "./assets/styles/web.css";
 
+
 Vue.use(Vuetify);
-const vuetify = new Vuetify({});
+const vuetify = new Vuetify({
+  // theme: {
+  //   dark : true,
+  //   themes: {
+  //     light: {
+  //       primary: "#fff",
+  //       accent: "#fff",
+  //       secondary: "#fff",
+  //       success: "#0f0",
+  //       info: "#00f",
+  //       warning: "#ff0",
+  //       error: "#f00"
+  //     },
+  //     dark : {
+  //       primary: "#121212",
+  //       accent: "#121212",
+  //       secondary: "#000",
+  //     }
+  //   },
+  // },
+});
 
 Vue.use(VueCookie);
 
